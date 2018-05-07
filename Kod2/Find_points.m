1f671ff4a6749a6949cc5acf0c16fda2ef3c2467
Stefan Pajka <stefpaj@o2.pl>
2018-05-07 03:40:26 +0200

    Gui ostateczne

--- Kod2/Find_points.m
+++
@@ -1,8 +0,0 @@
-function [  ] = Find_points( IR, VIS )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-[movingPoints,fixedPoints] =cpselect(IR,VIS,'Wait',true);
-save('Points.mat','movingPoints','fixedPoints')
-
-end
-
